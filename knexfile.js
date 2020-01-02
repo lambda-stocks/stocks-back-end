8d6446dd5dea759b4e342ea0f95193c3bac1b98f
Edward <lilkups@yahoo.com>
2020-01-02 17:14:01 -0600

    changed the db name

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,10 @@
-// Update with your config settings.
+const path = require("path");
 
 module.exports = {
-
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './database/users.sqlite3'
+      filename: path.resolve(__dirname,'database/lambdastocks.sqlite3')
     },
     useNullAsDefault: true
   },
