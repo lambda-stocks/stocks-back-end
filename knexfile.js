396d0fe7dbccc921bd4a2dde7a450e586ded6ca5
Edward <lilkups@yahoo.com>
2020-01-02 20:46:35 -0600

    production knex file

--- knexfile.js
+++ knexfile.js
@@ -8,4 +8,14 @@ module.exports = {
     },
     useNullAsDefault: true
   },
+  production: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.resolve(__dirname,'database/lambdastocks.sqlite3')
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    },
+    useNullAsDefault: true
+  },
 };
