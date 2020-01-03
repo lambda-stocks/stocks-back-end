38091433f2c0d9bf90e8755071fba9ebe63ced7c
Edward <lilkups@yahoo.com>
2020-01-02 20:21:45 -0600

    registration and login validaton

--- rest/index.js
+++ rest/index.js
@@ -1,10 +1,9 @@
+import  userController from './controllers/userController';
 
 export default app => {
-  const userRouter = require('./controllers/userRouter');
-
-  app.use('/api/auth', userRouter);
-
-  app.get('/', (req, res) => {
-    res.json({ message: "Server is up and running" })
-  })
+  app.route('/api').get((req, res) => {
+    res.json({ message: "Server is up and running" });
+  });
+  app.route('/api/login').post(userController.login);
+  app.route('/api/register').post(userController.register);
 };
